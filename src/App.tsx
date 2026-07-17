779ce85e908f000607c989530b297b7df281d94d
chzaeem47 <zaeemshahidak47@gmail.com>
2026-07-17 21:51:25 +0500

    Dynamic Port

--- src/App.tsx
+++ src/App.tsx
@@ -123,7 +123,7 @@ export default function App() {
     setSelectedHistoryItem(null);
 
     try {
-      const response = await fetch("http://localhost:3000/api/generate", {
+      const response = await fetch("/api/generate", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
